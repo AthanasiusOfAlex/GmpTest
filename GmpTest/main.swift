34ae9d21bca62b797403dd3d34f94ab85d7d27d6
Louis Melahn <lmelahn@legionaries.org>
2017-05-03 10:15:03 +0200

    Initial Commit

---
+++ GmpTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GmpTest
+//
+//  Created by Louis Melahn on 5/3/17.
+//  Copyright © 2017 Louis Melahn. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

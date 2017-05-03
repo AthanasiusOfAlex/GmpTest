45e756f106b197b541085d164c35cff829f22ef2
Louis Melahn <lmelahn@legionaries.org>
2017-05-03 13:10:13 +0200

    Before change to model.map

--- GmpTest/main.swift
+++ GmpTest/main.swift
@@ -11,5 +11,54 @@ import Gmp
 
 print("Hello, World!")
 
-var x, y, result: mpz_t;
+var x = mpz_t();
+var y = mpz_t();
+var result = mpz_t();
 
+__gmpz_init_set_str(&x, "7612058254738945", 10)
+__gmpz_init_set_str(&y, "2", 10)
+__gmpz_init(&result)
+
+__gmpz_mul(&result, &x, &y);
+
+
+extension mpz_t {
+    
+    func getString(base: Int) -> String {
+        
+        var buffer = [CChar]()
+        
+        var numberToGet = self
+        __gmpz_get_str(&buffer, Int32(base), &numberToGet)
+
+        return String(cString: buffer)
+        
+    }
+    
+    
+
+}
+
+extension mpz_t : CustomStringConvertible {
+    
+    public var description: String {
+        
+        return self.getString(base: 10)
+    
+    }
+    
+}
+
+
+
+print("    \(x)")
+print("*")
+print("    \(y)")
+print("--------------------")
+print(result)
+print()
+let base = 12
+print("In base \(base): \(result.getString(base: 16))")
+__gmpz_clear(&x)
+__gmpz_clear(&y)
+__gmpz_clear(&result)

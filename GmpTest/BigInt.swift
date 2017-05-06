8732764db2064a306117e926e44c1689ad5b5b0c
Louis Melahn <lmelahn@legionaries.org>
2017-05-06 21:04:39 +0200

    Compiles!!!

--- GmpTest/BigInt.swift
+++ GmpTest/BigInt.swift
@@ -9,144 +9,186 @@
 import Foundation
 import GMP
 
+extension String {
+
+    init(_ bigInt: BigInt, usingBase base: Int) {
+
+        var buffer = [CChar]()
+
+        var numberToGet = bigInt.internalStruct
+        __gmpz_get_str(&buffer, Int32(base), &numberToGet)
+
+        self = String(cString: buffer)
+
+    }
+
+    init(_ bigInt: BigInt) {
+
+        self = String(bigInt)
+
+    }
+
+}
+
+extension Int {
+    
+    init(_ bigInt: BigInt) {
+        
+        var numberToGet = bigInt.internalStruct
+        
+        if __gmpz_fits_slong_p(&numberToGet)==0 {
+            
+            fatalError("BigInt is too large to be converted to Int.")
+            
+        } else {
+            
+            self = __gmpz_get_si(&numberToGet)
+            
+        }
+        
+    }
+    
+}
+
+extension IntMax {
+    
+    init(_ bigInt: BigInt) {
+        
+        var numberToGet = bigInt.internalStruct
+        
+        if __gmpz_fits_slong_p(&numberToGet)==0 {
+        
+            fatalError("BigInt is too large to be converted to IntMax.")
+
+        } else {
+        
+            self = IntMax(__gmpz_get_si(&numberToGet))
+        
+        }
+        
+    }
+    
+}
+
+
+protocol SignedMaxable: SignedInteger {
+    
+    static var max: Self { get }
+    init?(_ value: String, radix: Int)
+
+}
+
+protocol UnsignedMaxable: UnsignedInteger {
+    
+    static var max: Self { get }
+    init?(_ value: String, radix: Int)
+
+}
+
+extension Int: SignedMaxable {}
+extension Int64: SignedMaxable {}
+extension Int32: SignedMaxable {}
+extension Int16: SignedMaxable {}
+extension Int8: SignedMaxable {}
+
+extension UInt: UnsignedMaxable {}
+extension UInt64: UnsignedMaxable {}
+extension UInt32: UnsignedMaxable {}
+extension UInt16: UnsignedMaxable {}
+extension UInt8: UnsignedMaxable {}
+
+
+extension SignedMaxable {
+    
+    init(_ bigInt: BigInt) {
+        
+        
+        guard bigInt < BigInt(Self.max) else {
+            
+            fatalError("Tried to convert BigInt greater than maximum allowed for \(Mirror(reflecting: Self.max).subjectType)")
+            
+        }
+        
+        let string = String(bigInt)
+        self.init(string, radix: 10)!
+        
+    }
+    
+}
+
+//extension Maxable {
+//    
+//    init(_ bigInt: BigInt) {
+//        
+//        //var numberToGet = bigInt.internalStruct
+//        
+//        if bigInt > BigInt(Maxable.max) {}
+//        
+//        let string = String(bigInt)
+//        
+//        
+//    }
+//}
+
+
+protocol GenericInteger: Integer {
+    init(_ value: BigInt)
+}
+
+
+
 final class BigInt {
     
     /// This is the internal GMP struct that actually holds the number.
-    fileprivate var internalMpStruct: mpz_t
+    fileprivate var internalStruct: mpz_t
     
     init() {
         
-        internalMpStruct = mpz_t()
-        __gmpz_init(&internalMpStruct)
+        internalStruct = mpz_t()
+        __gmpz_init(&internalStruct)
         
     }
     
-    init(_ string: String, usingBase base: Int) {
+    init(_ string: String, usingBase base: Int=10) {
         
         let buffer = string.cString(using: .ascii)!
         
-        internalMpStruct = mpz_t()
-        __gmpz_init_set_str(&internalMpStruct, buffer, Int32(base))
+        internalStruct = mpz_t()
+        __gmpz_init_set_str(&internalStruct, buffer, Int32(base))
         
     }
     
-    init(_ n: Int) {
-        
-        internalMpStruct = mpz_t()
-        __gmpz_init_set_si(&internalMpStruct, n)
-        
-    }
-    
-    init(_ n: IntMax) {
+    convenience init<T>(_ n: T) where T:SignedInteger {
         
         let string = String(n)
-        let cString = string.cString(using: .ascii)!
-        
-        internalMpStruct = mpz_t()
-        __gmpz_init_set_str(&internalMpStruct, cString, 10)
+        self.init(string)
         
     }
     
-    
-    init(_ n: UInt) {
+    convenience init<T>(_ n: T) where T:UnsignedInteger {
         
-        internalMpStruct = mpz_t()
-        __gmpz_init_set_ui(&internalMpStruct, n)
+        let string = String(n)
+        self.init(string)
         
     }
+
     
     init(_ n: mpz_t) {
         
-        internalMpStruct = n
+        internalStruct = n
         
     }
-    
-    convenience init(_ string: String) {
         
-        self.init(string, usingBase: 10)
-        
-    }
-    
     deinit {
         
-        __gmpz_clear(&internalMpStruct)
+        __gmpz_clear(&internalStruct)
         
     }
-    
-}
-
-
-extension BigInt: Equatable {
-    
-    fileprivate static func compare(_ lhs: BigInt, to rhs: BigInt) -> Int32 {
-        
-        var lhsStruct = lhs.internalMpStruct
-        var rhsStruct = rhs.internalMpStruct
-        
-        return __gmpz_cmp(&lhsStruct, &rhsStruct)
-        
-    }
-    
-    public static func ==(lhs: BigInt, rhs: BigInt) -> Bool {
-        
-        if compare(lhs, to: rhs)==0 {
-            
-            return true
-            
-        } else {
-            
-            return false
-            
-        }
-        
-    }
-    
-}
-
-extension BigInt: Comparable {
-    
-    public static func <(lhs: BigInt, rhs: BigInt) -> Bool {
-        
-        if compare(lhs, to: rhs) < 0 {
-            
-            return true
-            
-        } else {
-            
-            return false
-            
-        }
-        
-    }
-    
-}
-
-extension BigInt: IntegerArithmetic {
-    
-    /// Explicitly convert to `IntMax`, trapping on overflow (except in
-    /// -Ounchecked builds).
-    func toIntMax() -> IntMax {
-        
-        let max = BigInt(IntMax.max)
-        
-        if self > max {
-            
-            return IntMax.max
-            
-        } else {
-            
-            let string = String(self, usingBase: 10)
-            return IntMax(string)!
-            
-        }
-        
-    }
-    
     
     static func addWithOverflow(_ lhs: BigInt, _ rhs: BigInt) -> (BigInt, overflow: Bool) {
         
         let result = BigInt()
-        __gmpz_add(&result.internalMpStruct, &lhs.internalMpStruct, &rhs.internalMpStruct)
+        __gmpz_add(&result.internalStruct, &lhs.internalStruct, &rhs.internalStruct)
         return (result, overflow: false)
         
     }
@@ -154,7 +196,7 @@ extension BigInt: IntegerArithmetic {
     static func subtractWithOverflow(_ lhs: BigInt, _ rhs: BigInt) -> (BigInt, overflow: Bool) {
         
         let result = BigInt()
-        __gmpz_sub(&result.internalMpStruct, &lhs.internalMpStruct, &rhs.internalMpStruct)
+        __gmpz_sub(&result.internalStruct, &lhs.internalStruct, &rhs.internalStruct)
         return (result, overflow: false)
         
     }
@@ -162,7 +204,7 @@ extension BigInt: IntegerArithmetic {
     static func multiplyWithOverflow(_ lhs: BigInt, _ rhs: BigInt) -> (BigInt, overflow: Bool) {
         
         let result = BigInt()
-        __gmpz_mul(&result.internalMpStruct, &lhs.internalMpStruct, &rhs.internalMpStruct)
+        __gmpz_mul(&result.internalStruct, &lhs.internalStruct, &rhs.internalStruct)
         return (result, overflow: false)
         
     }
@@ -178,7 +220,7 @@ extension BigInt: IntegerArithmetic {
             
         } else {
             
-            __gmpz_tdiv_q(&result.internalMpStruct, &lhs.internalMpStruct, &rhs.internalMpStruct)
+            __gmpz_tdiv_q(&result.internalStruct, &lhs.internalStruct, &rhs.internalStruct)
             return (result, overflow: false)
             
         }
@@ -196,42 +238,128 @@ extension BigInt: IntegerArithmetic {
             
         } else {
             
-            __gmpz_tdiv_r(&result.internalMpStruct, &lhs.internalMpStruct, &rhs.internalMpStruct)
+            __gmpz_tdiv_r(&result.internalStruct, &lhs.internalStruct, &rhs.internalStruct)
             return (result, overflow: false)
             
         }
         
     }
     
-}
-
-extension BigInt: CustomStringConvertible {
+    fileprivate static func compare(_ lhs: BigInt, to rhs: BigInt) -> Int32 {
+        
+        var lhsStruct = lhs.internalStruct
+        var rhsStruct = rhs.internalStruct
+        
+        return __gmpz_cmp(&lhsStruct, &rhsStruct)
+        
+    }
     
-    public var description: String {
+    static func ==(lhs: BigInt, rhs: BigInt) -> Bool {
+        
+        if compare(lhs, to: rhs)==0 {
+            
+            return true
+            
+        } else {
+            
+            return false
+            
+        }
+        
+    }
+    
+    static func <(lhs: BigInt, rhs: BigInt) -> Bool {
+        
+        if compare(lhs, to: rhs) < 0 {
+            
+            return true
+            
+        } else {
+            
+            return false
+            
+        }
+        
+    }
+
+    func toIntMax() -> IntMax {
+        
+        return IntMax(self)
+        
+    }
+    
+    var description: String {
         
         return String(self, usingBase: 10)
         
     }
+
+
+    typealias IntegerLiteralType = Int
+    
+    convenience init(integerLiteral: IntegerLiteralType) {
+        
+        self.init(integerLiteral)
+        
+    }
+    
+
+
+}
+
+extension BigInt: Equatable, Comparable, IntegerArithmetic, CustomStringConvertible, ExpressibleByIntegerLiteral {}
+
+
+extension BigInt: Strideable {
+    
+    typealias Stride = IntMax
+    
+    func distance(to other: BigInt) -> Stride {
+        
+        return (other - self).toIntMax()
+        
+    }
+    
+    func advanced(by n: Stride) -> BigInt {
+        
+        return BigInt(n) + self
+        
+    }
     
 }
 
-extension String {
+extension BigInt: Hashable {
     
-    init(_ bigInt: BigInt, usingBase base: Int) {
+    var hashValue: Int {
         
-        var buffer = [CChar]()
-        
-        var numberToGet = bigInt.internalMpStruct
-        __gmpz_get_str(&buffer, Int32(base), &numberToGet)
-        
-        self = String(cString: buffer)
-        
-    }
-    
-    init(_ bigInt: BigInt) {
-        
-        self = String(bigInt)
+        let string = String(self)
+        return string.hashValue
         
     }
     
 }
+
+//extension BigInt: BitwiseOperations {
+//    /// Returns the intersection of bits set in the two arguments.
+//    ///
+//    /// The bitwise AND operator (`&`) returns a value that has each bit set to
+//    /// `1` where *both* of its arguments had that bit set to `1`. This is
+//    /// equivalent to the intersection of two sets. For example:
+//    ///
+//    ///     let x: UInt8 = 5        // 0b00000101
+//    ///     let y: UInt8 = 14       // 0b00001110
+//    ///     let z = x & y           // 0b00000100
+//    ///
+//    /// Performing a bitwise AND operation with a value and `allZeros` always
+//    /// returns `allZeros`.
+//    ///
+//    ///     print(x & .allZeros)    // 0b00000000
+//    ///     // Prints "0"
+//    ///
+//    /// - Complexity: O(1).
+//    static func &(lhs: BigInt, rhs: BigInt) -> Self {
+//        <#code#>
+//    }
+//
+//
+//}
--- GmpTest/main.swift
+++ GmpTest/main.swift
@@ -7,9 +7,77 @@
 //
 
 import Foundation
+import GMP
 
-let x = BigInt(Int.max)
-let y = BigInt(55)
 
-print("x: \(x); y: \(y); x+y: \(x+y); x*y: \(x*y); x/y: \(x/y); x%y: \(x%y)")
+protocol Junk {
+    
+    static func +(lhs: Self, rhs: Self) -> Self
+    static func *(lhs: Self, rhs: Self) -> Self
+    
+}
 
+
+extension Int: Junk {}
+extension Double: Junk {}
+
+func throwAway<T: Junk>(_ m: T, _ n: T) -> T {
+    
+    return m * n + n
+    
+}
+
+let ta = throwAway(5.5, 6.234324)
+let tb = throwAway(5, 6)
+let tc = throwAway(5.5, 6)
+
+print("Junk: \(ta), \(tb), \(tc)")
+
+func doSomething<T: Integer>(n: T) -> T {
+
+    print(Mirror(reflecting: n).subjectType)
+    
+    return 1
+    
+}
+
+let n8: Int8 = 5
+let n16: Int16 = 5
+let n32: Int32 = 5
+let n64: Int64 = 5
+
+print(doSomething(n: n8))
+print(doSomething(n: n16))
+print(doSomething(n: n32))
+print(doSomething(n: n64))
+
+
+
+
+//let x = BigInt(Int.max)
+//let y = BigInt(55)
+//let z: BigInt = 678
+//let zz: BigInt = "65535"
+//
+//print("zz: \(zz); in base 16: \(String(zz, usingBase: 16))")
+//print("in base 15: \(String(zz, usingBase: 15))")
+//
+//print("x: \(x); y: \(y); x+y: \(x+y); x*y: \(x*y); x/y: \(x/y); x%y: \(x%y)")
+//
+//var g = mpz_t()
+//var cStrG = [CChar]()
+//__gmpz_get_str(&cStrG, Int32(10), &g)
+//__gmpz_clear(&g)
+//__gmpz_clear(&g)
+
+
+//for i in BigInt("500000")..<BigInt("500100") {
+//
+//    print(i)
+//    
+//}
+
+print(IntMax.max)
+
+//_ = BigInt(UInt(234))
+print(Int("453534")!)
